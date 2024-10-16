d533af9c31be9b2d15fa6f497feaa33ba2485259
haxy <zaidsaheb4@gmail.com>
2024-10-17 01:08:13 +0200

    initial commit with navbar and hero

---
+++ app/homepage/herosection.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import Image from "next/image";
+
+export default function HeroSection() {
+  const images = ["/giphy1.webp", "/giphy2.webp", "/giphy3.webp", "/giphy4.webp"];
+
+  return (
+    <>
+      <div className="flex flex-col center justify-center overflow-hidden relative ">
+        <div className="absolute inset-0 bg-black opacity-50 z-10"></div>
+        {/* 1. Adjust the container */}
+        <div className="w-full h-[40vh] overflow-hidden relative">
+          {/* 2. Create the animating container */}
+          <div className="flex items-center h-full absolute left-0 animate-slide whitespace-nowrap">
+            {/* 3. Map through images twice */}
+            {[...images, ...images].map((src, index) => (
+              <div key={index} className="inline-block h-full">
+                <Image
+                  src={src}
+                  alt={`Hero ${index + 1}`}
+                  width={300}
+                  height={300}
+                  className="w-[25vw] h-full object-cover"
+                />
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+      <div className="absolute flex flex-col justify-center items-center top-[15vh] w-[100vw]">
+        <Image
+          src="/cpl.svg"
+          alt="Logo"
+          width={350}
+          height={350}
+          className="z-20 invert"
+        />
+        <h1 className="text-white text-4xl font-bold z-20">Cyber Physical Labs</h1>
+      </div>
+    </>
+  );
+}
