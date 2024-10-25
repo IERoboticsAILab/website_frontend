418f88be4d56fbba8a189cac5a0a7d4c5d777bec
haxy <zaidsaheb4@gmail.com>
2024-10-25 21:08:37 +0200

    made type consistent

--- app/homepage/projectsection.tsx
+++ app/homepage/projectsection.tsx
@@ -2,10 +2,10 @@
 import ProjectCard from '@/components/projectcard';
 import axios from 'axios';
 import { useState, useEffect } from 'react';
-import { Projects } from '@/types/project';
+import { Project } from '@/types/project';
 
 export default function ProjectsSection() {
-  const [projects, setProjects] = useState<Projects[]>([]);
+  const [projects, setProjects] = useState<Project[]>([]);
 
   useEffect(() => {
     const fetchProjects = async () => {
@@ -16,21 +16,21 @@ export default function ProjectsSection() {
       };
 
       try {
-        const res = await axios.get(url, { headers });
-        const projectsWithFullImageUrls = res.data.data.map((project: Projects) => ({
+        const res = await axios.get<{ data: Project[] }>(url, { headers });
+        const projectsData = res.data.data.map(project => ({
           ...project,
-          gallery: project.gallery?.map((image: { formats?: { thumbnail?: { url: string } } }) => ({
+          gallery: project.gallery?.map(image => ({
             ...image,
-            formats: image.formats ? {
+            formats: {
               ...image.formats,
-              thumbnail: image.formats.thumbnail ? {
+              thumbnail: {
                 ...image.formats.thumbnail,
                 url: `${imgurl}${image.formats.thumbnail.url}`
-              } : undefined
-            } : undefined
+              }
+            }
           }))
         }));
-        setProjects(projectsWithFullImageUrls);
+        setProjects(projectsData);
       } catch (error) {
         console.error("Error fetching projects data:", error);
       }
@@ -44,10 +44,10 @@ export default function ProjectsSection() {
       <h1 className="text-3xl font-bold mb-8">Our Projects</h1>
       <div className="overflow-x-auto pb-4">
         <div className="flex space-x-6" style={{ width: `${projects.length * 20}%` }}>
-          {projects.map((project: Projects) => (
+          {projects.map(project => (
             <div key={project.id} className="w-64 h-64 flex-shrink-0">
               <ProjectCard
-                id={project.id}
+                id={project.id.toString()}
                 title={project.name}
                 description={project.tagline}
                 imageUrl={project.gallery?.[0]?.formats?.thumbnail?.url || ''}
