091d168af9c4daf8e89c8ad8abd3cde8ee53df34
haxy <zaidsaheb4@gmail.com>
2024-10-25 22:34:23 +0200

    fetches everything except publications

--- app/projects/[id]/page.tsx
+++ app/projects/[id]/page.tsx
@@ -1,71 +1,89 @@
 import { notFound } from 'next/navigation';
+import axios from 'axios';
+import { useState, useEffect } from 'react';
 import Navbar from '@/components/navbar';
 import Footer from '@/components/footer';
 import Image from 'next/image';
+import { Project, Projects } from '@/types/project';
 
-// This function would typically fetch data from an API or database
-async function getProject(id: string) {
 
-  // Simulating an API call
-  const projects = [
-    { id: '1', title: 'Project 1', description: 'Full description of Project 1', imageUrl: '/project1.jpg' },
-    { id: '2', title: 'Project 2', description: 'Full description of Project 2', imageUrl: '/project2.jpg' },
-    { id: '3', title: 'Project 3', description: 'Full description of Project 3', imageUrl: '/project3.jpg' },
-    { id: '4', title: 'Project 4', description: 'Full description of Project 4', imageUrl: '/project4.jpg' },
-    { id: '5', title: 'Project 5', description: 'Full description of Project 5', imageUrl: '/project5.jpg' },
-    { id: '6', title: 'Project 6', description: 'Full description of Project 6', imageUrl: '/project6.jpg' },
-    // Add more projects as needed
-  ];
-  return projects.find(p => p.id === id);
+interface ProjectPageProps {
+  params: { id: string };
 }
 
-export default async function ProjectPage({ params }: { params: { id: string } }) {
-  const project = await getProject(params.id);
 
+export default async function ProjectPage({ params }: ProjectPageProps) {
+  const url = `${process.env.NEXT_PUBLIC_STRAPI_API_URL}/projects?populate[members][populate][0]=profilepic&populate[banner]=*&populate[gallery]=*`;
+  const headers = {
+    Authorization: `Bearer ${process.env.NEXT_PUBLIC_STRAPI_API_KEY}`,
+  };
+
+  let projects: Projects;
+  try {
+    const res = await axios.get(url, { headers });
+    projects = res.data;
+  } catch (error) {
+    console.error("Error fetching projects data:", error);
+    notFound();
+  }
+
+  const project = projects?.data.find(p => p.id.toString() === params.id);
   if (!project) {
     notFound();
   }
 
-  const members = [
-    { name: "Wall-E", image: "/walle.jpg" },
-    { name: "Wall-E", image: "/walle.jpg" },
-    { name: "Wall-E", image: "/walle.jpg" },
-    { name: "Wall-E", image: "/walle.jpg" },
-  ];
+
 
   return (
     <div>
       <Navbar />
-      <div className="relative">
+      <div className="">
         <div className="h-96 bg-gray-700 relative">
           <img
-            src={project.imageUrl}
-            alt={project.title}
+            src={`${process.env.NEXT_PUBLIC_STRAPI_API_URL_IMG}${project.banner.url}`}
+            alt={project.name}
             className="w-full h-full object-cover"
           />
           <div className="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center">
-            <h1 className="text-4xl font-bold text-white">{project.title}</h1>
+            <h1 className="text-4xl font-bold text-white">{project.name}</h1>
           </div>
         </div>
       </div>
-      <div className="container mx-auto px-4 py-8">
+      <div className="mx-auto px-4 py-8 h-full">
         <div className="flex flex-col md:flex-row gap-8">
-          <div className="md:w-1/2">
-            <h2 className="text-2xl font-bold mb-4">About</h2>
-            <p className="text-lg">{project.description}</p>
-            <iframe width="500" height="200" src="https://www.youtube.com/embed/SoZiZE2Zcng" title="Hedgehog Boat" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" ></iframe>
+          <div className="md:w-1/2 h-full">
+            <p className="text-sm text-gray-600 mb-2">{project.date}</p>
+            <h2 className="text-5xl font-bold mb-4 text-gray-900">{project.name}</h2>
+            <p className="text-xl text-gray-700 mb-6 leading-relaxed">{project.tagline}</p>
+            <p className="text-gray-800">{project.members.map(member => member.firstname + " " + member.lastnames).join(', ')}</p>
+            <div className="relative w-full pt-[56.25%]">
+              <iframe
+                className="absolute top-0 left-0 w-full h-full"
+                src={project.videolink}
+                title="Hedgehog Boat"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              ></iframe>
+            </div>
           </div>
-          <div className="md:w-1/2">
-            <h2 className="text-2xl font-bold mb-4">Details</h2>
-            <p className="text-lg">Additional project information can go here. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+          <div className="md:w-1/2 h-full">
+            <h2 className="text-2xl font-bold mb-4">About</h2>
+            {project.about?.map((textItem, i) => (
+                textItem.children?.map((child, j) => (
+                  textItem.type === "paragraph" ? (
+                    <p key={`${i}-${j}`}>{child.text}</p>
+                  ) : textItem.type === "heading" ? (
+                    <h2 key={`${i}-${j}`} className="text-2xl font-semibold mb-4">{child.text}</h2>
+                  ) : null
+                ))
+              ))}
           </div>
         </div>
 
-        <div className="mt-12">
+        {/* <div className="mt-12">
           <h2 className="text-3xl font-bold mb-4">Publication</h2>
           <div className="bg-gray-100 p-4 rounded-lg">
             <p className="text-sm text-gray-600">IEU 2024</p>
-            <h3 className="text-xl font-semibold my-2">{project.title}</h3>
+            <h3 className="text-xl font-semibold my-2">{project.name}</h3>
             <p className="text-gray-700">Author 1, Author 2, Author 3</p>
             <div className="mt-4 flex space-x-4">
               <a href="#" className="flex items-center text-blue-600 hover:underline">
@@ -83,17 +101,16 @@ export default async function ProjectPage({ params }: { params: { id: string } }
               </a>
             </div>
           </div>
-        </div>
+        </div> */}
 
         <div className="mt-12">
           <h2 className="text-3xl font-bold mb-4">Gallery</h2>
           <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
-            {/* Replace these with actual project images */}
-            {[1, 2, 3, 4, 5, 6, 7, 8].map((i) => (
-              <div key={i} className="aspect-square bg-gray-200 rounded-lg overflow-hidden">
+            {project?.gallery?.map((image) => (
+              <div key={image.id} className="aspect-square bg-gray-200 rounded-lg overflow-hidden">
                 <Image
-                  src={`/placeholder-image-${i}.jpg`}
-                  alt={`Gallery image ${i}`}
+                  src={`${process.env.NEXT_PUBLIC_STRAPI_API_URL_IMG}${image.url}`}
+                  alt={image.name}
                   width={300}
                   height={300}
                   className="w-full h-full object-cover"
@@ -104,20 +121,20 @@ export default async function ProjectPage({ params }: { params: { id: string } }
         </div>
 
         <div className="mt-12">
-          <h2 className="text-3xl font-bold mb-8">AxLab Members</h2>
+          <h2 className="text-3xl font-bold mb-8">CyPhyLife Members</h2>
           <div className="grid grid-cols-2 md:grid-cols-4 gap-8">
-            {members.map((member) => (
-              <div key={member.name} className="flex flex-col items-center">
+            {project.members.map((member) => (
+              <div key={member.firstname + " " + member.lastnames} className="flex flex-col items-center">
                 <div className="w-32 h-32 rounded-full overflow-hidden mb-4">
                   <Image
-                    src={member.image}
-                    alt={member.name}
+                    src={`${process.env.NEXT_PUBLIC_STRAPI_API_URL_IMG}${member?.profilepic?.url}`}
+                    alt={member.firstname + " " + member.lastnames}
                     width={128}
                     height={128}
                     className="w-full h-full object-cover"
                   />
                 </div>
-                <p className="text-center font-semibold">{member.name}</p>
+                <p className="text-center font-semibold">{member.firstname + " " + member.lastnames}</p>
               </div>
             ))}
           </div>
