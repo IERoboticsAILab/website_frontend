0f34921619e8d0ecd4b94609e3d644ef5c049e3e
haxy <zaidsaheb4@gmail.com>
2024-10-28 00:29:47 +0100

    added config here

--- env.d.ts
+++ env.d.ts
@@ -1,5 +1,7 @@
 declare namespace NodeJS {
   interface ProcessEnv {
+    NEXT_PUBLIC_STRAPI_API_URL: string;
+    NEXT_PUBLIC_STRAPI_API_URL_IMG: string;
     NEXT_PUBLIC_STRAPI_API_KEY: string;
   }
 }
