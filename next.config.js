da0db71ac4dc629a1bc6db75f0b8552f2ab4f0d7
haxy <zaidsaheb4@gmail.com>
2024-10-28 01:28:24 +0100

    this might work

--- next.config.js
+++ next.config.js
@@ -1,10 +1,14 @@
 /** @type {import('next').NextConfig} */
-/** @type {import('./envConfig.ts')} */
 
-const apiUrl = new URL(process.env.NEXT_PUBLIC_STRAPI_API_URL_IMG);
 const nextConfig = {
   images: {
-    domains: ['localhost', apiUrl.hostname], // Add other domains if needed
+    domains: ['localhost'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
   },
   // ... other configurations
 }
