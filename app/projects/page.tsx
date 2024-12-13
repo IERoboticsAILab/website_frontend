ddf8e6a37f153a0b405edece2a704de6c68b2c58
haxybaxy <zaidsaheb4@gmail.com>
2024-12-13 15:15:57 +0000

    adding new stuff

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -31,15 +31,48 @@ async function getProjects() {
   }));
 }
 
+async function getResearchLines() {
+  const url = `${process.env.NEXT_PUBLIC_STRAPI_API_URL}/research-lines?populate=*`;
+  const imgurl = `${process.env.NEXT_PUBLIC_STRAPI_API_URL_IMG}`;
+  const headers = {
+    Authorization: `Bearer ${process.env.NEXT_PUBLIC_STRAPI_API_KEY}`,
+  };
+
+  const res = await fetch(url, { headers });
+  if (!res.ok) {
+    throw new Error('Failed to fetch research lines');
+  }
+
+  const data = await res.json();
+  return data.data.map((researchLine: Project) => ({
+    ...researchLine,
+    gallery: researchLine.gallery?.map((image) => ({
+      ...image,
+      formats: image.formats ? {
+        ...image.formats,
+        thumbnail: image.formats.thumbnail ? {
+          ...image.formats.thumbnail,
+          url: `${imgurl}${image.formats.thumbnail.url}`
+        } : undefined
+      } : undefined
+    }))
+  }));
+}
+
+
 export default async function ProjectsPage() {
   const projects = await getProjects();
+  const researchLines = await getResearchLines();
+
+  const combinedItems = [...projects, ...researchLines];
+
 
   return (
     <div>
       <Navbar />
       <div className="px-6 mx-4 py-8 min-h-screen">
         <h1 className="text-3xl font-bold mb-8">Projects</h1>
-        <ProjectsGrid projects={projects} />
+        <ProjectsGrid projects={combinedItems} />
       </div>
       <Footer />
     </div>
