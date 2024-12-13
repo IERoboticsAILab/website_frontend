94ea7a88036a76fd5ba4694c8b2f70997308ca7b
haxy <zaidsaheb4@gmail.com>
2024-12-13 18:22:23 +0100

    only if it exists

--- app/publications/page.tsx
+++ app/publications/page.tsx
@@ -51,12 +51,14 @@ export default async function PublicationsPage() {
                 </Link>
                 <p className="text-sm text-gray-700 mb-4">{pub.authors}</p>
                 <div className="flex gap-2 mb-4">
+                  {pub.pdflink && (
                   <a href={pub.pdflink} className="flex items-center gap-1 text-blue-600 hover:underline">
                     <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
                       <path fillRule="evenodd" d="M4 4a2 2 0 012-2h4.586A2 2 0 0112 2.586L15.414 6A2 2 0 0116 7.414V16a2 2 0 01-2 2H6a2 2 0 01-2-2V4zm2 6a1 1 0 011-1h6a1 1 0 110 2H7a1 1 0 01-1-1zm1 3a1 1 0 100 2h6a1 1 0 100-2H7z" clipRule="evenodd" />
                     </svg>
                     Pre-Print
                   </a>
+                  )}
                   {pub.paperlink && (
                     <a href={pub.paperlink} className="flex items-center gap-1 text-blue-600 hover:underline" target="_blank" rel="noopener noreferrer">
                       <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
